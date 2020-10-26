880bbb8996fc83f97aa7d1d61dc3aa9cb07a82da
Kriston Costa <kriston@Kristons-MacBook-Pro.local>
2020-10-26 18:42:34 -0400

    Initial basic framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import {add_two_ints, fib} from "wasm";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [sum, setSum] = useState<number>(add_two_ints(10, 20));
+   const [fibb, setFib] = useState<number>(fib(10));
+   return (
+      <div className="App" >
+         <div>Sum Results: {sum}</div>
+         <div>Fib Results: {fibb}</div>
+      </div>
+   );
 }
 
 export default App;
---
+++ src/services/serve.tsx
@@ -0,0 +1,5 @@
+import {add_two_ints} from "wasm";
+
+export function add() {
+    return add_two_ints(1, 2);
+}
